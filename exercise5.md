c2cf745ba984c5f7551c221552530c03e12ecf66
Piotr Jachowicz <jachowicz.piotr@gmail.com>
2023-07-18 11:36:06 +0200

    Exercise 5 - instructions

---
+++ exercise5.md
@@ -0,0 +1,14 @@
+We will expand on the code from previous example, and add a bit more data to our account creation. We will also
+store the accounts in an array.
+
+First, we will add a new enum - AccountType with two values: DEBIT and CREDIT.
+We will ask for the account type from the user in addition to the data we've asked for earlier.
+
+Then we will store the account in an array and ask the user for another account. After this one is also stored in the 
+array, we will use the index notation (`result[0]`) to display the values. Don't worry about the repetition, we will 
+optimize that code in next exercises!
+
+The `index.ts` file already contains some empty methods with comments on what needs to be implemented. Please follow those instructions, 
+and run `npm run start` to test the solution.
+
+Happy coding!
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,22 @@
 import promptInit from 'prompt-sync';
 const prompt = promptInit();
 
+// Add the AccountType enum
+
 console.log('Welcome to the Typescript Accounting System!');
 console.log('Please add your first account');
 
-// Prompt the user for the account name and the monthly budget for this account - uncomment the two lines at the top and use const value = prompt('Please provide the value')
-// Remember to parse the monthly budget to a Number, i.e. using Number(value)
 const name = prompt('Please provide the account name:');
 const monthlyBudget = Number(prompt('Please provide the monthly budget for this account:'));
+// Prompt the user for the account type
 
-// When you have the values, create an object out of them
+// When you have the values, create an object out of them - add the account type
 const account = {
   name,
   monthlyBudget,
 };
 
-// Shorter way:
-// const account = {
-//   name = prompt('Please provide the account name:'),
-//   monthlyBudget = Number(prompt('Please provide the monthly budget for this account:')),
-// };
-
-// Then, use console.log to display the values of the properties
-console.log('The account you have added is:');
-console.log(`Name: ${account.name}`);
-console.log(`Monthly budget: ${account.monthlyBudget}`);
+// create the result array and push the account
+// const result = [];
+// repeat the process for a second account
+// log the results to the console
